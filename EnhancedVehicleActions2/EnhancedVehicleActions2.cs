7ad0ce5861dacf2bb9f7f43196bcddca1124f4a0
Harrison Kung <20KungHarrison@iusd.org>
2020-03-15 16:57:01 -0700

    First update

---
+++ EnhancedVehicleActions2/EnhancedVehicleActions2.cs
@@ -0,0 +1,414 @@
+﻿using Rage;
+
+[assembly: Rage.Attributes.Plugin("EnhancedVehicleActions2", Description = "Enhanced vehicle actions and more", Author = "holexion")]
+
+namespace EnhancedVehicleActions2
+{
+    public class EnhancedVehicleActions2
+    {
+        //iniFile contains player keybindings
+        public static InitializationFile iniFile = new InitializationFile("plugins/EnhancedVehicleActions2.ini");
+        public static void Main()
+        {
+            //grabs options from iniFile
+            bool isTireRetainmentEnabled = iniFile.ReadBoolean("Options", "tireRetainment", true);
+            bool isSpeedometerEnabled = iniFile.ReadBoolean("Options", "toggleableSpeedometer", true);
+            //bool isInventoryEnabled = iniFile.ReadBoolean("Options", "carInventory", true);
+            //bool isSoundEnabled = iniFile.ReadBoolean("Options", "blinkerSounds", true);
+
+            if (isTireRetainmentEnabled)
+            {
+                TireRetainment();
+            }
+            if (isSpeedometerEnabled)
+            {
+                Speedometer();
+            }
+            /*
+            if (isInventoryEnabled)
+            {
+                program.Inventory();
+            }
+            if (isSoundEnabled)
+            {
+                program.Sound();
+            }
+            */
+
+            VehicleIndicators(); //activates vehicle indicators
+            KeyPolling(); //activates key polling
+            _ = new Menu(); //activates menu
+        }
+        public static void KeyPolling()
+        {
+            //grabs keybindings from iniFile
+            System.Type keysType = typeof(System.Windows.Forms.Keys);
+            System.Windows.Forms.Keys rightBlinkerKey = (System.Windows.Forms.Keys)iniFile.ReadEnum(keysType, "KeyBindings", "RightBlinker", System.Windows.Forms.Keys.OemCloseBrackets);
+            System.Windows.Forms.Keys leftBlinkerKey = (System.Windows.Forms.Keys)iniFile.ReadEnum(keysType, "KeyBindings", "LeftBlinker", System.Windows.Forms.Keys.OemOpenBrackets);
+            System.Windows.Forms.Keys hazardsKey = (System.Windows.Forms.Keys)iniFile.ReadEnum(keysType, "KeyBindings", "Hazards", System.Windows.Forms.Keys.OemPipe);
+
+            //grabs controller keybindings from iniFile
+            System.Type controllerType = typeof(ControllerButtons);
+            //ControllerButtons altKey = (ControllerButtons)iniFile.ReadEnum(controllerType, "ControllerKeyBindings", "AlternativeKey", ControllerButtons.A);
+            ControllerButtons contRightBlinkerKey = (ControllerButtons)iniFile.ReadEnum(controllerType, "ControllerKeyBindings", "RightBlinker", ControllerButtons.RightShoulder);
+            ControllerButtons contLeftBlinkerKey = (ControllerButtons)iniFile.ReadEnum(controllerType, "ControllerKeyBindings", "LeftBlinker", ControllerButtons.LeftShoulder);
+
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+                    /*
+                    if (Game.IsKeyDown(windowKey))// || (Game.IsControllerButtonDown(contTrunkKey) && Game.IsControllerButtonDownRightNow(altKey)))
+                    {
+                        Game.LocalPlayer.Character.CurrentVehicle.Windows[0].Remove();
+                        //Game.LocalPlayer.Character.CurrentVehicle.Windows[0].();
+                    }
+                    if (Game.IsKeyDown(frontDoorKey) || (Game.IsControllerButtonDown(contFrontDoorKey) && Game.IsControllerButtonDownRightNow(altKey)))
+                    {
+                        ActivateVehicleDoors(true);
+                    }
+                    if (Game.IsKeyDown(backDoorKey) || (Game.IsControllerButtonDown(contBackDoorKey) && Game.IsControllerButtonDownRightNow(altKey)))
+                    {
+                        ActivateVehicleDoors(false);
+                    }
+                    */
+                    if (Game.IsKeyDown(rightBlinkerKey) || (Game.IsControllerButtonDown(contRightBlinkerKey))) //&& !Game.IsControllerButtonDown(altKey)))
+                    {
+                        intendedStatus = VehicleIndicatorLightsStatus.RightOnly;
+                    }
+                    if (Game.IsKeyDown(leftBlinkerKey) || (Game.IsControllerButtonDown(contLeftBlinkerKey))) //&& !Game.IsControllerButtonDown(altKey)))
+                    {
+                        intendedStatus = VehicleIndicatorLightsStatus.LeftOnly;
+                    }
+                    if (Game.IsKeyDown(hazardsKey) || (Game.IsControllerButtonDown(contRightBlinkerKey) && Game.IsControllerButtonDownRightNow(contLeftBlinkerKey))) //&& !Game.IsControllerButtonDown(altKey)))
+                    {
+                        intendedStatus = VehicleIndicatorLightsStatus.Both;
+                    }
+                    if (Game.LocalPlayer.Character.IsGettingIntoVehicle) //if player is getting into vehicle, activate
+                    {
+                        ToggleDefaultRadio(); //change radio to default
+                    }
+                    GameFiber.Yield();
+                }
+            });
+        }
+
+        public static void TireRetainment()
+        {
+            float steeringAngle = 0;
+            bool saveSteeringLocation = false;
+
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+                    Vehicle playerVehicle = Game.LocalPlayer.Character.CurrentVehicle;
+                    Vehicle lastPlayerVehicle = Game.LocalPlayer.Character.LastVehicle;
+
+                    if (playerVehicle.Exists() && playerVehicle.Speed < 1 && !saveSteeringLocation)
+                    {
+                        if (Game.IsControlPressed(1, GameControl.VehicleMoveLeftOnly) || Game.IsControlPressed(1, GameControl.VehicleMoveRightOnly))
+                        {
+                            steeringAngle = playerVehicle.SteeringAngle * 1.2f;
+                        }
+                        else if (!Game.IsControlPressed(1, GameControl.VehicleMoveLeftOnly) && !Game.IsControlPressed(1, GameControl.VehicleMoveRightOnly))
+                        {
+                            playerVehicle.SteeringAngle = steeringAngle;
+                            saveSteeringLocation = true;
+                        }
+                    }
+                    else if (playerVehicle.Exists() && (playerVehicle.Speed > 1 || Game.IsControlPressed(1, GameControl.VehicleMoveLeftOnly) || Game.IsControlPressed(1, GameControl.VehicleMoveRightOnly)))
+                    {
+                        saveSteeringLocation = false;
+                        steeringAngle = 0f;
+                    }
+                    else if (saveSteeringLocation)
+                    {
+                        if (playerVehicle.Exists())
+                        {
+                            playerVehicle.SteeringAngle = steeringAngle;
+                        }
+                        else if (lastPlayerVehicle.Exists())
+                        {
+                            lastPlayerVehicle.SteeringAngle = steeringAngle;
+                        }
+                    }
+                    GameFiber.Yield();
+                }
+            });
+        }
+
+        //isSpeedometerEnabled activates/deactivates Speedometer() through in-game Menu
+        public static bool isSpeedometerEnabled = false;
+        private static void Speedometer()
+        {
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+                    if (isSpeedometerEnabled && Game.LocalPlayer.Character.IsInAnyVehicle(false))
+                    {
+                        float vehicleSpeed = Game.LocalPlayer.Character.CurrentVehicle.Speed * 2.23694f;
+                        Game.DisplaySubtitle("Speed: " + System.Math.Round(vehicleSpeed).ToString() + " MPH   Gear: " + Game.LocalPlayer.Character.CurrentVehicle.CurrentGear);
+                    }
+                    GameFiber.Yield();
+                }
+            });
+        }
+
+        public static bool isInteriorLightsEnabled = false;
+        public static void ToggleInteriorLights()
+        {
+            Vehicle vehicle;
+
+            if (Game.LocalPlayer.Character.CurrentVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.CurrentVehicle;
+            }
+            else if (Game.LocalPlayer.Character.LastVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.LastVehicle;
+            }
+            else
+            {
+                Game.LogTrivialDebug("Error: Valid vehicle not detected");
+                return;
+            }
+
+            if (isInteriorLightsEnabled)
+            {
+                vehicle.IsInteriorLightOn = true;
+            }
+            else
+            {
+                vehicle.IsInteriorLightOn = false;
+            }
+            Game.LogTrivialDebug("Interior Lights Activated");
+        }
+
+        public static int intendedRadio = -1;
+        public static void ToggleDefaultRadio()
+        {
+            Vehicle vehicle;
+
+            if (Game.LocalPlayer.Character.CurrentVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.CurrentVehicle;
+            }
+            else
+            {
+                Game.LogTrivialDebug("Error: Valid vehicle not detected");
+                return;
+            }
+
+            if (Menu.isRadioStationEnabled)
+            {
+                Game.LogTrivialDebug("RadioEnabled");
+                vehicle.RadioStation = (RadioStation)intendedRadio;
+            }
+        }
+
+        public static bool isAlarmEnabled = false;
+        public static void ToggleAlarm()
+        {
+            Vehicle vehicle;
+
+            if (Game.LocalPlayer.Character.CurrentVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.CurrentVehicle;
+            }
+            else if (Game.LocalPlayer.Character.LastVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.LastVehicle;
+            }
+            else
+            {
+                Game.LogTrivialDebug("Error: Valid vehicle not detected");
+                return;
+            }
+
+            if (isAlarmEnabled)
+            {
+                vehicle.AlarmTimeLeft = System.TimeSpan.FromMinutes(5);
+            }
+            else
+            {
+                vehicle.AlarmTimeLeft = System.TimeSpan.Zero;
+            }
+            Game.LogTrivialDebug("Alarm Activated");
+        }
+
+        //Activates door using index
+        public static void ActivateVehicleDoors(int index)
+        {
+            Vehicle vehicle;
+
+            if (Game.LocalPlayer.Character.CurrentVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.CurrentVehicle;
+            }
+            else if (Game.LocalPlayer.Character.LastVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.LastVehicle;
+            }
+            else
+            {
+                Game.LogTrivialDebug("Error: Valid vehicle not detected");
+                return;
+            }
+
+            if (vehicle.Doors[index].IsValid())
+            {
+                if (vehicle.Doors[index].IsOpen)
+                {
+                    vehicle.Doors[index].Close(false);
+                }
+                else
+                {
+                    vehicle.Doors[index].Open(false);
+                }
+            }
+        }
+        public static void ActivateVehicleDoors()
+        {
+            Vehicle vehicle;
+
+            if (Game.LocalPlayer.Character.CurrentVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.CurrentVehicle;
+            }
+            else if (Game.LocalPlayer.Character.LastVehicle.Exists())
+            {
+                vehicle = Game.LocalPlayer.Character.LastVehicle;
+            }
+            else
+            {
+                return;
+            }
+
+            if (CheckAllDoorsOpen(vehicle))
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    if (vehicle.Doors[i].IsValid())
+                    {
+                        ActivateVehicleDoors(i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    if (vehicle.Doors[i].IsValid() && !vehicle.Doors[i].IsOpen)
+                    {
+                        ActivateVehicleDoors(i);
+                    }
+                }
+            }
+        }
+
+        //Checks if all doors in a vehicle have the same state (all doors are opened/closed) returns true if yes, false if no
+        private static bool CheckAllDoorsOpen(Vehicle vehicle)
+        {
+            bool areAllDoorsOpen = true;
+            for (int i = 0; i < 6; i++)
+            {
+                if (vehicle.Doors[i].IsValid())
+                {
+                    if (!vehicle.Doors[i].IsOpen)
+                    {
+                        areAllDoorsOpen = false;
+                    }
+                }
+            }
+            return areAllDoorsOpen;
+        }
+
+        static VehicleIndicatorLightsStatus status = VehicleIndicatorLightsStatus.Off;
+        static VehicleIndicatorLightsStatus intendedStatus = VehicleIndicatorLightsStatus.Off;
+        static float initialHeading = 0f;
+        static uint turnOffAt = 0;
+        private static void VehicleIndicators()
+        {
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+                    Vehicle playerVehicle = Game.LocalPlayer.Character.CurrentVehicle;
+                    if (playerVehicle.Exists())
+                    {
+                        if (intendedStatus == VehicleIndicatorLightsStatus.RightOnly && playerVehicle.IsEngineOn) //right blinker activates with close bracket
+                        {
+                            Game.LogTrivialDebug("Right Blinker activated");
+                            turnOffAt = 0;
+                            if (status == VehicleIndicatorLightsStatus.RightOnly)
+                            {
+                                status = VehicleIndicatorLightsStatus.Off;
+                            }
+                            else
+                            {
+                                status = VehicleIndicatorLightsStatus.RightOnly;
+                                initialHeading = playerVehicle.Heading;
+                            }
+                            playerVehicle.IndicatorLightsStatus = status;
+                        }
+                        else if (intendedStatus == VehicleIndicatorLightsStatus.LeftOnly && playerVehicle.IsEngineOn) //left blinker activates with close bracket
+                        {
+                            Game.LogTrivialDebug("Left Blinker activated");
+                            turnOffAt = 0;
+                            if (status == VehicleIndicatorLightsStatus.LeftOnly)
+                            {
+                                status = VehicleIndicatorLightsStatus.Off;
+                            }
+                            else
+                            {
+                                status = VehicleIndicatorLightsStatus.LeftOnly;
+                                initialHeading = playerVehicle.Heading;
+                            }
+                            playerVehicle.IndicatorLightsStatus = status;
+                        }
+                        else if (intendedStatus == VehicleIndicatorLightsStatus.Both) //left blinker activates with close bracket
+                        {
+                            Game.LogTrivialDebug("Hazards activated");
+                            if (status == VehicleIndicatorLightsStatus.Both)
+                            {
+                                status = VehicleIndicatorLightsStatus.Off;
+                            }
+                            else
+                            {
+                                status = VehicleIndicatorLightsStatus.Both;
+                            }
+                            playerVehicle.IndicatorLightsStatus = status;
+                        }
+
+                        if (status != VehicleIndicatorLightsStatus.Both)
+                        {
+                            if (turnOffAt == 0u)
+                            {
+                                if (status != VehicleIndicatorLightsStatus.Off)
+                                {
+                                    float heading = playerVehicle.Heading;
+                                    if (System.Math.Abs(heading - initialHeading) > 60.0f)
+                                    {
+                                        turnOffAt = Game.GameTime + 1500;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (Game.GameTime >= turnOffAt)
+                                {
+                                    status = VehicleIndicatorLightsStatus.Off;
+                                    playerVehicle.IndicatorLightsStatus = status;
+                                }
+                            }
+                        }
+                    }
+                    intendedStatus = VehicleIndicatorLightsStatus.Off;
+                    GameFiber.Yield();
+                }
+            });
+        }
+    }
+}
---
+++ EnhancedVehicleActions2/Menu.cs
@@ -0,0 +1,148 @@
+﻿using Rage;
+using RAGENativeUI;
+using RAGENativeUI.Elements;
+
+namespace EnhancedVehicleActions2
+{
+    class Menu
+    {
+        private static GameFiber MenusProcessFiber;
+        private static UIMenu mainMenu;
+        private static MenuPool _menuPool;
+        private static UIMenuCheckboxItem speedometerCheckbox;
+        private static UIMenuCheckboxItem alarmKeyCheckbox;
+        private static UIMenuCheckboxItem interiorLightKeyCheckbox;
+        private static UIMenuListItem vehicleDoorsList;
+        private static UIMenuListItem radioStationList;
+
+        //iniFile information gathered
+        private static readonly InitializationFile iniFile = EnhancedVehicleActions2.iniFile; //grabs iniFile from EnhancedVehicleActions2
+        private readonly bool isSpeedometerEnabled = iniFile.ReadBoolean("Options", "toggleableSpeedometer", true);
+        public static readonly bool isRadioStationEnabled = iniFile.ReadBoolean("Options", "toggleableRadioStation", true);
+        private static readonly System.Windows.Forms.Keys actionKey = (System.Windows.Forms.Keys)iniFile.ReadEnum(typeof(System.Windows.Forms.Keys), "KeyBindings", "ActionKey", System.Windows.Forms.Keys.F7);
+        private static readonly ControllerButtons controllerActionKey = (ControllerButtons)iniFile.ReadEnum(typeof(ControllerButtons), "ControllerKeyBindings", "ControllerActionKey", ControllerButtons.RightThumb);
+
+        public Menu()
+        {
+            MenusProcessFiber = new GameFiber(MainLogic);
+            _menuPool = new MenuPool();
+            mainMenu = new UIMenu("Vehicle Actions", "");
+            _menuPool.Add(mainMenu);
+
+            //Add items to mainMenu
+            if (isSpeedometerEnabled) //checks if speedometer is enabled to add to pool
+            {
+                mainMenu.AddItem(speedometerCheckbox = new UIMenuCheckboxItem("Speedometer", false, "Enables/Disables Speedometer"));
+            }
+            mainMenu.AddItem(alarmKeyCheckbox = new UIMenuCheckboxItem("Car Alarm", false, "Toggles Car Alarm"));
+            mainMenu.AddItem(interiorLightKeyCheckbox = new UIMenuCheckboxItem("Interior Light", false, "Toggles Interior Light"));
+            mainMenu.AddItem(vehicleDoorsList = new UIMenuListItem("Vehicle Doors", "Select which door of your vehicle to open and close", "All", "Front left", "Front right", "Rear left", "Rear right", "Hood", "Trunk"));
+            if (isRadioStationEnabled) //checks if radio station is enabled to add to pool
+            {
+                string[] radioList = new string[] { "Off", "Los Santos Rock Radio", "Non-Stop-Pop FM", "Radio Los Santos", "Channel X", "West Coast Talk Radio", "Rebel Radio", "Soulwax FM", "East Los FM", "West Coast Classics", "Blue Ark", "Worldwide FM", "FlyLo FM", "The Lowdown 91.1", "The Lab", "Radio Mirror Park", "Space 103.2", "Vinewood Boulevard Radio", "Blonded Los Santos 97.8", "Los Santos Underground", "Self Radio"};
+                mainMenu.AddItem(radioStationList = new UIMenuListItem("Radio", "Sets default radio station to be used on all vehicles when entered", radioList));
+            }
+
+            mainMenu.OnCheckboxChange += OnCheckboxChange;
+            mainMenu.OnItemSelect += OnItemSelect;
+            mainMenu.OnListChange += OnListChange;
+
+            MenusProcessFiber.Start();
+            GameFiber.Hibernate();
+        }
+
+        public static void MainLogic()
+        {
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+                    if (Game.IsKeyDown(actionKey))
+                    {
+                        mainMenu.Visible = !mainMenu.Visible;
+                    }
+                    if (Game.IsControllerButtonDownRightNow(controllerActionKey))
+                    {
+                        mainMenu.Visible = true;
+                    }
+                    _menuPool.ProcessMenus();
+                    GameFiber.Yield();
+                }
+            });
+        }
+
+        public static void OnCheckboxChange(UIMenu sender, UIMenuCheckboxItem checkbox, bool Checked)
+        {
+            if (sender == mainMenu)
+            {
+                if (checkbox == speedometerCheckbox)
+                {
+                    if (Checked)
+                    {
+                        EnhancedVehicleActions2.isSpeedometerEnabled = true;
+                    }
+                    else
+                    {
+                        EnhancedVehicleActions2.isSpeedometerEnabled = false;
+                        Game.DisplaySubtitle("");
+                    }
+                }
+                else if (checkbox == alarmKeyCheckbox)
+                {
+                    EnhancedVehicleActions2.isAlarmEnabled = Checked;
+                    EnhancedVehicleActions2.ToggleAlarm();
+                }
+                else if (checkbox == interiorLightKeyCheckbox)
+                {
+                    EnhancedVehicleActions2.isInteriorLightsEnabled = Checked;
+                    EnhancedVehicleActions2.ToggleInteriorLights();
+                }
+            }
+        }
+
+        public static void OnListChange(UIMenu sender, UIMenuListItem list, int index)
+        {
+            if (sender == mainMenu && list == radioStationList)
+            {
+                EnhancedVehicleActions2.intendedRadio = index - 1;
+                EnhancedVehicleActions2.ToggleDefaultRadio();
+               //iniFile.Write("Options", "defaultRadioStation", "None");
+            }
+        }
+
+        public static void OnItemSelect(UIMenu sender, UIMenuItem selectedItem, int index)
+        {
+            if (sender == mainMenu && selectedItem == vehicleDoorsList)
+            {
+                if (vehicleDoorsList.SelectedItem.DisplayText == "All")
+                {
+                    EnhancedVehicleActions2.ActivateVehicleDoors();
+                }
+                else if (vehicleDoorsList.SelectedItem.DisplayText == "Front left")
+                {
+                    EnhancedVehicleActions2.ActivateVehicleDoors(0);
+                }
+                else if (vehicleDoorsList.SelectedItem.DisplayText == "Front right")
+                {
+                    EnhancedVehicleActions2.ActivateVehicleDoors(1);
+                }
+                else if (vehicleDoorsList.SelectedItem.DisplayText == "Rear left")
+                {
+                    EnhancedVehicleActions2.ActivateVehicleDoors(2);
+                }
+                else if (vehicleDoorsList.SelectedItem.DisplayText == "Rear right")
+                {
+                    EnhancedVehicleActions2.ActivateVehicleDoors(3);
+                }
+                else if (vehicleDoorsList.SelectedItem.DisplayText == "Hood")
+                {
+                    EnhancedVehicleActions2.ActivateVehicleDoors(4);
+                }
+                else if (vehicleDoorsList.SelectedItem.DisplayText == "Trunk")
+                {
+                    EnhancedVehicleActions2.ActivateVehicleDoors(5);
+                }
+            }
+        }
+    }
+}
